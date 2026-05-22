74be16d6fe1ea39597a9d433f837af45e12bcebe
Maximilian Rudolph <rudolphmax@mailbox.org>
2026-05-22 16:40:17 +0200

    feat(Labeller): implemented ziviani questionnaire

--- labeller/app.py
+++ labeller/app.py
@@ -1,15 +1,16 @@
 import csv
 from pathlib import Path
-from typing import cast
+from typing import Union, cast
 
+import numpy as np
 import pandas as pd
 import streamlit as st
 from PIL import Image, ImageOps
 from streamlit.delta_generator import DeltaGenerator
 
-from utils import prepare_label_csv
+from utils import prepare_label_csv, preprocess_image
 
-cols = ["Num", "Filename", "Reader", "Spelling", "Effort", "Layout", "Letter Formation", "Alteration"]
+cols = ["Num", "Filename", "Reader", "Legibility", "Effort", "Layout", "Letter Formation", "Alteration", "Formation Ziv", "Horizontal Alignment", "Vertical Alignment"]  # noqa: E501
 
 if not Path("data/validation_set.csv").is_file():
   raise Exception("No validation dataset csv found.")
@@ -23,8 +24,20 @@ labels = pd.read_csv("data/validation_labels.csv", sep=",")
 start_index = 0
 num_documents = samples.shape[0]
 
+if "round" not in st.session_state:
+    st.session_state["round"] = 0
+
 if labels.shape[0] > 0:
-  start_index = labels.shape[0]
+  if (labels.shape[0] < samples.shape[0]):
+    start_index = labels.shape[0]
+    st.session_state.round = 0
+  else:
+    c = labels.loc[np.isnan(labels["Formation Ziv"])]
+    if len(c) > 0:
+      start_index = c.index[0]
+      st.session_state.round = 1
+    else:
+      st.session_state.round = 3
 
 if "index" not in st.session_state:
     st.session_state["index"] = start_index
@@ -32,10 +45,13 @@ if "index" not in st.session_state:
 if "reader" not in st.session_state:
     st.session_state["reader"] = labels.iloc[start_index-1]["Reader"]
 
+if "guidelines" not in st.session_state:
+    st.session_state["guidelines"] = "None"
+
 def next_doc(values: dict) -> None:
   sample = samples.iloc[st.session_state.index]
 
-  row = [
+  labels.loc[st.session_state.index] = [
     sample["Num"],
     sample["Filename"],
     values["reader"],
@@ -44,24 +60,39 @@ def next_doc(values: dict) -> None:
     values["layout"],
     values["letter-formation"],
     values["alteration"],
+    values["formation-ziv"],
+    values["hor-alignment"],
+    values["vert-alignment"],
   ]
 
-  with Path("data/validation_labels.csv").open("a", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow(row)
+  labels.to_csv("data/validation_labels.csv", index=False)
 
-  st.session_state.index += 1
+  if (st.session_state.index < num_documents-1):
+    st.session_state.index += 1
+  else:
+    st.session_state.index = 0
+
   st.rerun()
 
-def question(container: DeltaGenerator, name: str, desc: str, key: str) -> int:
+def switch_guidelines() -> None:
+  st.session_state.guidelines = st.session_state.guideline_switch
+
+def field(container: DeltaGenerator, name: str) -> DeltaGenerator:
   left, right = container.columns([2, 3])
   left.markdown("###### " + name)
+
+  return right
+
+def question(container: DeltaGenerator, name: str, desc: str, key: str) -> int:
+  right = field(container, name)
 
   return right.slider(desc, 1, 5, 1, key=key)
 
 st.set_page_config(layout="wide")
 
-if st.session_state.index > num_documents-1:
+st.write(st.session_state.guidelines)
+
+if st.session_state.round == 3:
   st.header("You're Done!")
   st.text("That's it! All " + str(num_documents) + " documents are labelled. You can now close this window.")
   st.balloons()
@@ -78,55 +109,161 @@ else:
 
     img = Image.open(sample["Filename"])
     img = ImageOps.exif_transpose(img)
-    st.image(image=cast("Image.Image", img))
+    img, img_vert, img_hor = preprocess_image(img)
+
+    if (st.session_state.guidelines == "Horizontal"):
+      st.image(image=cast("Image.Image", img_hor))
+    elif (st.session_state.guidelines == "Vertical"):
+      st.image(image=cast("Image.Image", img_vert))
+    else:
+      st.image(image=cast("Image.Image", img))
 
     st.session_state.reader = st.text_input("Reader", labels.iloc[start_index-1]["Reader"], width=100)
 
   with right_column:
     st.space("medium")
-    with st.form("hls_questionnaire", clear_on_submit=True):
-      q = st.container(gap="medium")
 
-      legibility = question(q, "Legibility", "Overall, how legible is the text when first reading?", "legibility")
-      effort = question(
-        q,
-        "Effort",
-        "How much effort is required for you to read the document overall?",
-        "effort",
-      )
-      layout = question(
-        q,
-        "Layout",
-        """An overall impression of the layout of writing
-        on the page. Well organised handwriting is consistent, with elements
-        appropriately positioned in relation to each other (e.g. the position of the margin,
-        placement of letters on the baseline, spaces within and between words).""",
-        "layout",
-      )
-      letter_formation = question(
-        q,
-        "Letter Formation",
-        """An overall impression of letter formation. Well formed letters are appropriately shaped,
-        contain all necessary elements, neat letter closures and are consistent in size and slope.""",
-        "letter-formation",
-      )
-      alteration = question(
-        q,
-        "Alteration",
-        """An overall impression of the attempts made to rectify letters within words. Includes the
-        addition of elements, re-tracing  or re-writing of letters.""",
-        "alteration",
+    if (st.session_state.round == 0):
+      with st.form("hls_questionnaire", clear_on_submit=True, enter_to_submit=False):
+        q = st.container(gap="medium")
+
+        legibility = question(q, "Legibility", "Overall, how legible is the text when first reading?", "legibility")
+        effort = question(
+          q,
+          "Effort",
+          "How much effort is required for you to read the document overall?",
+          "effort",
+        )
+        layout = question(
+          q,
+          "Layout",
+          """An overall impression of the layout of writing
+          on the page. Well organised handwriting is consistent, with elements
+          appropriately positioned in relation to each other (e.g. the position of the margin,
+          placement of letters on the baseline, spaces within and between words).""",
+          "layout",
+        )
+        letter_formation = question(
+          q,
+          "Letter Formation",
+          """An overall impression of letter formation. Well formed letters are appropriately shaped,
+          contain all necessary elements, neat letter closures and are consistent in size and slope.""",
+          "letter-formation",
+        )
+        alteration = question(
+          q,
+          "Alteration",
+          """An overall impression of the attempts made to rectify letters within words. Includes the
+          addition of elements, re-tracing  or re-writing of letters.""",
+          "alteration",
+        )
+
+        button_container = st.container(horizontal=True, horizontal_alignment="right")
+
+        submitted = button_container.form_submit_button("Next")
+        if submitted:
+          next_doc({
+            "reader": st.session_state.reader,
+            "legibility": legibility,
+            "effort": effort,
+            "layout": layout,
+            "letter-formation": letter_formation,
+            "alteration": alteration,
+            "formation-ziv": np.nan,
+            "hor-alignment": np.nan,
+            "vert-alignment": np.nan,
+          })
+
+    elif (st.session_state.round == 1):
+      st.write(
+        """
+        This is the second questionnaire. Familiarize yourself with the letter formation chart below.
+        If needed, Use the guideline selector to enable guidelines.
+        """,
       )
 
-      button_container = st.container(horizontal=True, horizontal_alignment="right")
+      with st.expander("Letter Formation Chart", expanded=True):
+        st.image("presentations/assets/zivianiEvaluationHandwritingPerformance1984_table2.png")
 
-      submitted = button_container.form_submit_button("Next")
-      if submitted:
-        next_doc({
-          "reader": st.session_state.reader,
-          "legibility": legibility,
-          "effort": effort,
-          "layout": layout,
-          "letter-formation": letter_formation,
-          "alteration": alteration,
-        })
+      st.segmented_control(
+          "Guidelines", [ "None", "Horizontal", "Vertical" ],
+          default=st.session_state.guidelines,
+          selection_mode="single",
+          key="guideline_switch",
+          on_change=switch_guidelines,
+      )
+
+      with st.form("ziv_questionnaire", clear_on_submit=True, enter_to_submit=False):
+        q = st.container(gap="medium")
+
+        r = field(q, "Number of Letters")
+        num_letters = r.number_input(
+          "Count the number of letters in the document (a-z, not including symbols).",
+          min_value=0,
+          value=1,
+        )
+
+        r = field(q, "Formation")
+        formation_ziv = r.number_input(
+          "Count the number of malformed letters according to the provided chart.",
+          min_value=0,
+          value=0,
+        )
+
+        r = field(q, "Horizontal Alignment")
+
+        with r.expander("Edit"):
+          st.caption(
+            """
+            Enable the horizontal guidelines. Count the spaces between all words in terms of space between the provided
+            guidelines. For example, two words may be separated by 2, 0.5, or 1.5 spaces. Make one entry below for each
+            word-gap.
+            """,
+          )
+          hor_alignment = st.data_editor(
+            pd.DataFrame(columns=["Hor Spacing"]),  # pyright: ignore[reportArgumentType]
+            column_config={
+              "Hor Spacing": st.column_config.NumberColumn(
+                step=0.1,
+              ),
+            },
+            num_rows="dynamic",
+            hide_index=True,
+          )
+
+        r = field(q, "Vertical Alignment")
+
+        with r.expander("Edit"):
+          st.caption(
+            """
+            Enable the vertical guidelines. Count the spaces between all lines in terms of space between the provided
+            guidelines. For example, two lines may be separated by 2, 0.5, or 1.5 spaces. Make one entry below for each
+            line-gap.
+            """,
+          )
+          vert_alignment = st.data_editor(
+            pd.DataFrame(columns=["Vert Spacing"]),  # pyright: ignore[reportArgumentType]
+            column_config={
+              "Vert Spacing": st.column_config.NumberColumn(
+                step=0.1,
+              ),
+            },
+            num_rows="dynamic",
+            hide_index=True,
+          )
+
+        button_container = st.container(horizontal=True, horizontal_alignment="right")
+        submitted = button_container.form_submit_button("Next")
+
+        if submitted:
+          next_doc({
+            "reader": labels.iloc[st.session_state.index]["Reader"],
+            "legibility": labels.iloc[st.session_state.index]["Legibility"],
+            "effort": labels.iloc[st.session_state.index]["Effort"],
+            "layout": labels.iloc[st.session_state.index]["Layout"],
+            "letter-formation": labels.iloc[st.session_state.index]["Letter Formation"],
+            "alteration": labels.iloc[st.session_state.index]["Alteration"],
+            "formation-ziv": 1 - (formation_ziv / num_letters),
+            "hor-alignment": hor_alignment["Hor Spacing"].std() / hor_alignment["Hor Spacing"].mean(),
+            "vert-alignment": vert_alignment["Vert Spacing"].std() / vert_alignment["Vert Spacing"].mean(),
+          })
--- labeller/utils.py
+++ labeller/utils.py
@@ -2,6 +2,15 @@ from __future__ import annotations
 
 import csv
 from pathlib import Path
+from typing import TYPE_CHECKING
+
+import cv2
+import numpy as np
+from PIL import Image
+
+if TYPE_CHECKING:
+  from streamlit.delta_generator import DeltaGenerator
+  from PIL.Image import Image as ImageType
 
 
 # Create a csv file for storing labels
@@ -11,3 +20,25 @@ def prepare_label_csv(cols: list[str]) -> None:
   with p.open("w", newline="") as f:
     writer = csv.writer(f)
     writer.writerow(cols)
+
+def preprocess_image(img: ImageType) -> tuple[ImageType, ImageType, ImageType]:
+  width, height = img.size
+  dpi = img.info.get("dpi", (72, 72))
+
+  # Load image
+  image_vert_lines = cv2.cvtColor(np.array(img.copy()), cv2.COLOR_RGB2BGR)
+  image_hor_lines = cv2.cvtColor(np.array(img.copy()), cv2.COLOR_RGB2BGR)
+
+  line_offset = (2.5 / 2.54) * dpi[0]
+
+  line_x = 0
+  while line_x < width:
+    cv2.line(image_vert_lines, (int(line_x), 0), (int(line_x), height), (255, 0, 0), 2)
+    line_x += line_offset
+
+  line_y = 0
+  while line_y < height:
+    cv2.line(image_hor_lines, (0, int(line_y)), (width, int(line_y)), (255, 0, 0), 2)
+    line_y += line_offset
+
+  return img, Image.fromarray(image_vert_lines), Image.fromarray(image_hor_lines)
